bc344c0cf12e430ce6c182aac7d439b277fb26b4
JudithVCQ <judith.ccahuana@unmsm.edu.pe>
2024-11-01 23:29:03 -0500

    Sesion

--- src/gestionveterinaria/Usuario.java
+++ src/gestionveterinaria/Usuario.java
@@ -1,5 +1,35 @@
 package gestionveterinaria;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Usuario {
-    
+    private Conexion conexion;
+
+    public Usuario(Conexion conexion) {
+        this.conexion = conexion;
+    }
+
+    public boolean iniciarSesion(String usuario, String contrasena) {
+        String sql = "SELECT * FROM Usuario WHERE usuario = ? AND contrasena = ?";
+        try (PreparedStatement stmt = conexion.conectar().prepareStatement(sql)) {
+            stmt.setString(1, usuario);
+            stmt.setString(2, contrasena);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                System.out.println("Inicio de sesión exitoso.");
+                return true;
+            } else {
+                System.out.println("Usuario o contraseña incorrectos.");
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
+
+
